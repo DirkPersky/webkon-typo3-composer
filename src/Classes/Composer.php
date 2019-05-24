5a3a2fe9d03fed4092815ff3b56a9074b29e0ca1
Dirk Persky <dearic@gmx.net>
2019-05-24 09:10:06 +0200

    Update Composer.php

--- src/Classes/Composer.php
+++ src/Classes/Composer.php
@@ -60,7 +60,9 @@ class Composer {
         putenv("COMPOSER_HOME={$config['COMPOSER_HOME']}");
         putenv("COMPOSER={$config['COMPOSER']}composer.json" );
         putenv("OSTYPE={$config['OSTYPE']}"); //force to use php://output instead of php://stdout
-
+        // Update Composer config#
+        exec(trim(sprintf('cd %1$s && composer config platform.php %2$d.%3$d.%4$d', $config['COMPOSER'], PHP_MAJOR_VERSION, PHP_MINOR_VERSION, PHP_RELEASE_VERSION)));
+        // run Composer Command
         exec(trim(sprintf('cd %1$s && composer %2$s %3$s', $config['COMPOSER'], $command, $this->get('options'))), $out,$return);
         die(json_encode($out));
     }
